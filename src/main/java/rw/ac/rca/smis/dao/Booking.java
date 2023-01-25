37d005d2ae20ac05e2c91b3c7d141691d6d03e81
Ingabire Divine <divineingabire69@gmail.com>
2023-01-25 13:32:38 +0200

    implementation

--- src/main/java/rw/ac/rca/smis/dao/Booking.java
+++ src/main/java/rw/ac/rca/smis/dao/Booking.java
@@ -1,7 +1,10 @@
 package rw.ac.rca.smis.dao;
 
+import org.hibernate.Session;
+
 public interface Booking {
     Booking addBooking(Booking booking);
+
     Booking removeBooking(Boolean option);
     Booking upDateBooking(Booking booking);
     Booking GetBooking(int booking.id);
