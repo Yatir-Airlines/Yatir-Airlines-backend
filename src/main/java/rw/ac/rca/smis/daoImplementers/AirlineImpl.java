e2cbb331eba45ef17352a16516840dbfed6a11c4
SoniaKarigirwa <96382385+SoniaKarigirwa@users.noreply.github.com>
2023-02-06 09:31:19 +0200

    Implementation of AirlineImpl and AirportImpl

--- src/main/java/rw/ac/rca/smis/daoImplementers/AirlineImpl.java
+++ src/main/java/rw/ac/rca/smis/daoImplementers/AirlineImpl.java
@@ -3,30 +3,74 @@ package rw.ac.rca.smis.daoImplementers;
 import rw.ac.rca.smis.dao.AirlineDao;
 import rw.ac.rca.smis.orm.Airline;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 public class AirlineImpl implements AirlineDao {
+    private SessionFactory sessionFactory;
+
+    public AirlineImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public Airline getAirline(int airlineId) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Airline airline = (Airline) session.get(Airline.class, airlineId);
+        transaction.commit();
+        session.close();
+        return airline;
     }
+
     @Override
     public Set<Airline> getAirLines() {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Set<Airline> airlines = new HashSet<>(session.createCriteria(Airline.class).list());
+        transaction.commit();
+        session.close();
+        return airlines;
     }
 
     @Override
     public Airline deleteAirline(int airlineId) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Airline airline = (Airline) session.get(Airline.class, airlineId);
+        if (airline != null) {
+            session.delete(airline);
+        }
+        transaction.commit();
+        session.close();
+        return airline;
     }
 
     @Override
     public Airline updateAirline(int airlineId) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Airline airline = (Airline) session.get(Airline.class, airlineId);
+        if (airline != null) {
+            // update the airline object
+            session.update(airline);
+        }
+        transaction.commit();
+        session.close();
+        return airline;
     }
 
     @Override
     public Airline createAirline(Airline airLine) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(airLine);
+        transaction.commit();
+        session.close();
+        return airLine;
     }
 }
--- src/main/java/rw/ac/rca/smis/daoImplementers/AirportImpl.java
+++ src/main/java/rw/ac/rca/smis/daoImplementers/AirportImpl.java
@@ -1,33 +1,84 @@
 package rw.ac.rca.smis.daoImplementers;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import rw.ac.rca.smis.dao.AirportDao;
 import rw.ac.rca.smis.orm.Airport;
 
 import java.util.Set;
 
 public class AirportImpl implements AirportDao {
+    private SessionFactory sessionFactory;
+
+    public AirportImpl(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public Airport getAirport(int id) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Airport airport = (Airport) session.get(Airport.class, id);
+
+        transaction.commit();
+        session.close();
+
+        return airport;
     }
 
     @Override
     public Set<Airport> getAirports() {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Set<Airport> airports = (Set<Airport>) session.createCriteria(Airport.class).list();
+
+        transaction.commit();
+        session.close();
+
+        return airports;
     }
 
     @Override
     public Airport updateAirport(int airportId) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Airport airport = (Airport) session.get(Airport.class, airportId);
+        session.update(airport);
+
+        transaction.commit();
+        session.close();
+
+        return airport;
     }
 
     @Override
     public Airport deleteAirport(int airportId) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Airport airport = (Airport) session.get(Airport.class, airportId);
+        session.delete(airport);
+
+        transaction.commit();
+        session.close();
+
+        return airport;
     }
 
     @Override
     public Airport createAirport(Airport airport) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.save(airport);
+
+        transaction.commit();
+        session.close();
+
+        return airport;
     }
 }
